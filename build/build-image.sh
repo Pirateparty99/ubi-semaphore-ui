8ee86db9a568148a1ef999142a194de6773f4f22
Pirateparty99 <29783316+Pirateparty99@users.noreply.github.com>
2026-09-18 19:05:17 -0500

    Added logic to concatenate minimal Docker build stage to origin Dockerfile

--- build/build-image.sh
+++ build/build-image.sh
@@ -7,9 +7,13 @@ FROM registry.access.redhat.com/ubi8/python-312
 
 git clone https://github.com/semaphoreui/semaphore.git
 
-cd semaphore/deployment/docker/server/Dockerfile
+ORIGIN_DOCKERFILE=semaphore/deployment/docker/server/Dockerfile/Dockerfile
 
 echo "Updating image base for Docker build from Alpine to UBI $UBI_VERSION with Python version $PYTHON_VERSION:"
-sed -i Dockerfile -e "%s/
+sed -i $ORIGIN_DOCKERFILE -e "%s/
+
+echo "Updating 
 
 echo "Adding build step to use minimal UBI for built image:"
+
+cat Dockerfile-minimal >> $ORIGIN_DOCKERFILE
