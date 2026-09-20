35bc30a4e423c1eebd979a68a10ee20f6aedeb6b
Pirateparty99 <29783316+Pirateparty99@users.noreply.github.com>
2026-09-20 01:37:05 -0500

    Cleanup

--- build/build-image.sh
+++ build/build-image.sh
@@ -3,16 +3,16 @@
 UBI_VERSION=9.8
 PYTHON_VERSION=3.12
 
-FROM registry.access.redhat.com/ubi8/python-312
 
 git clone https://github.com/semaphoreui/semaphore.git
 
 ORIGIN_DOCKERFILE=semaphore/deployment/docker/server/Dockerfile/Dockerfile
 
 echo "Updating image base for Docker build from Alpine to UBI $UBI_VERSION with Python version $PYTHON_VERSION:"
-sed -i $ORIGIN_DOCKERFILE -e "%s/
+sed -i $ORIGIN_DOCKERFILE -e "%s/image tag/FROM registry.access.redhat.com/ubi8/python-312 as builder/g"
 
-echo "Updating 
+
+echo "Updating image to use glibc instead of MUSL as well as updating the package manager"
 
 echo "Adding build step to use minimal UBI for built image:"
 
